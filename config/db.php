2d26c57096f5ea2c1809e9f0604d0339a9f5c132
tlehmann7 <32707823+tlehmann7@users.noreply.github.com>
2017-12-06 16:37:56 +0100

    added mail function: from attribute

--- config/db.php
+++ config/db.php
@@ -470,7 +470,7 @@
 				
 				$headers[] = 'MIME-Version: 1.0';
 				$headers[] = 'Content-type: text/html; charset=iso-8859-1';
-				
+				$headers[] = 'From: einschreibung@fontaneum.de';
 				return mail($rmail, $email_caption."Passwort vergessen", getFilledTemplate($mail_link_to_script.$key), implode("\r\n", $headers));
 			}
 			else
